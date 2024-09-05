ac3a2bab502a5c5c8097cd5f9c349a8a90baa30e
Kiet Pham <kietpham.dev@gmail.com>
2024-09-05 17:19:35 +1000

    Domain Entities

---
+++ SM-Post/Post.Cmd/Post.Cmd.Domain/Entities/PostEntity.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Post.Cmd.Domain.Entities;
+
+[Table("Post")]
+public class PostEntity
+{
+    [Key]
+    public Guid PostId { get; set; }
+    public string Author { get; set; }
+    public DateTime DatePosted { get; set; }
+    public string Message { get; set; }
+    public int Liked { get; set; }
+    public virtual ICollection<CommentEntity> Comments { get; set; }
+}
